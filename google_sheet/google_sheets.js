e20476c1e8eaefc0d55e77a9bf5317988a03cea2
Manish Kasera <kaseram@amazon.com>
2022-03-19 18:44:07 +0530

    Adding data at individual mf level in a new sheet

--- google_sheet/google_sheets.js
+++ google_sheet/google_sheets.js
@@ -6,7 +6,6 @@ const {HEADERS, dataCurrencyToNumber, numberToA1Index, getSheetsService} = requi
 let firstDataRowIndex = 3; // added spark lines awesomeness in second row, so data starts from 3rd now
 let spreadsheetId = process.env.GOOGLE_SPREADSHEET_ID;
 let spreadSheetInfo;
-let sheetInfo;
 let sheetsService = getSheetsService();
 
 async function getSpreadSheetInfo() {
@@ -14,23 +13,16 @@ async function getSpreadSheetInfo() {
     return res.data;
 }
 
-async function readRange(range) {
+async function readRange(sheetInfo, range) {
     let response = await sheetsService.spreadsheets.values.get({spreadsheetId: spreadsheetId, range: `${sheetInfo.title}!${range}`});
     return response.data.values;
 }
 
-async function getLastUpdatedDate(firstDataRowIndex) {
-    let rangeValues = await readRange(`A${firstDataRowIndex}:A${firstDataRowIndex}`);
-    let lastUpdatedDate = rangeValues && rangeValues[0][0];
-    log("Sheets: last updated on => ", lastUpdatedDate)
-    return lastUpdatedDate;
+async function getHeadersFromSheet(sheetInfo, length) {
+    return await readRange(sheetInfo, 'A1:A' + length);
 }
 
-async function getHeadersFromSheet() {
-    return await readRange('A1:A' + HEADERS.length);
-}
-
-async function updateRow(index, data) {
+async function updateRow(sheetInfo, index, data) {
     return await sheetsService.spreadsheets.values.update({
         spreadsheetId: spreadsheetId,
         range: `${sheetInfo.title}!A${index}:${numberToA1Index(data.length)}${index}`,
@@ -39,7 +31,7 @@ async function updateRow(index, data) {
     })
 }
 
-async function insertRow(index, data) {
+async function insertRow(sheetInfo, index, data, pk) {
     let inheritFromBefore = index !== 0;
     // insert empty row at given index
     await sheetsService.spreadsheets.batchUpdate({spreadsheetId: spreadsheetId, resource: {
@@ -53,34 +45,68 @@ async function insertRow(index, data) {
                     },
                     inheritFromBefore: inheritFromBefore
                 }
+            },{
+                createDeveloperMetadata : {
+                    developerMetadata: {
+                        location: {
+                            dimensionRange: {
+                                sheetId: sheetInfo.sheetId,
+                                startIndex: index - 1,
+                                endIndex: index,
+                                dimension: "ROWS"
+                            }
+                        },
+                        metadataKey: 'pk',
+                        metadataValue: pk,
+                        visibility: "DOCUMENT"
+                    },
+                }
             }]
         }
     });
+
     if(data.length > 0) {
-        await updateRow(index, data);
+        await updateRow(sheetInfo, index, data);
     }
 }
 
-async function addHeaders() {
-    let headers = await getHeadersFromSheet();
-    if(!headers) {
+async function addHeaders(sheetInfo, headers) {
+    let headersSheet = await getHeadersFromSheet(sheetInfo, headers.length);
+    if(!headersSheet) {
         log("Sheets: Adding headers..")
-        await insertRow(1, HEADERS);
+        await insertRow(sheetInfo, 1, headers);
     }
 }
 
-async function addData(data) {
-    let lastUpdatedDate = await getLastUpdatedDate(firstDataRowIndex);
-    if(lastUpdatedDate && lastUpdatedDate === data[0]) {
+async function searchForPrimaryKey(sheetInfo, pk) {
+    return await sheetsService.spreadsheets.developerMetadata.search({
+        spreadsheetId,
+        requestBody: {
+            dataFilters: [
+                {
+                    developerMetadataLookup: {
+                        locationType: 'ROW',
+                        metadataKey: 'pk',
+                        metadataValue: pk
+                    }
+                }
+            ]
+        }
+    });
+}
+
+async function addData(sheetInfo, data, pk) {
+    let searchResult = await searchForPrimaryKey(sheetInfo, pk);
+    if (searchResult.data.matchedDeveloperMetadata) {
         log("in update block...");
-        await updateRow(firstDataRowIndex, data);
+        await updateRow(sheetInfo, searchResult.data.matchedDeveloperMetadata[0].developerMetadata.location.dimensionRange.endIndex, data);
     } else {
         log("in insert block...")
-        await insertRow(firstDataRowIndex, data);
+        await insertRow(sheetInfo, firstDataRowIndex, data, pk);
     }
 }
 
-function prepareData(datetime, numbers) {
+function prepareAggregateData(datetime, numbers) {
     let data = arrayToMap(numbers);
     data = dataCurrencyToNumber(data);
     // TODO: take en-IN from a env variable
@@ -94,13 +120,38 @@ function prepareData(datetime, numbers) {
     return orderedDataArray;
 }
 
-async function addToGoogleSheet(datetime, numbers) {
+async function addToGoogleSheet(datetime, data) {
     spreadSheetInfo = await getSpreadSheetInfo();
-    sheetInfo = spreadSheetInfo.sheets[process.env.GOOGLE_SHEET_INDEX].properties;
+    let aggregateSheetInfo = spreadSheetInfo.sheets[process.env.AGGREGATE_GOOGLE_SHEET_INDEX].properties;
+    let headers = HEADERS;
 
-    let orderedDataArray = prepareData(datetime, numbers);
-    await addHeaders();
-    await addData(orderedDataArray);
+    let orderedDataArray = prepareAggregateData(datetime, data.aggregateData);
+    await addHeaders(aggregateSheetInfo, headers);
+    await addData(aggregateSheetInfo, orderedDataArray, orderedDataArray[0]);
+
+    //add granular mf data
+    log("Updating mf data..");
+    let mfSheetInfo = spreadSheetInfo.sheets[process.env.MF_GOOGLE_SHEET_INDEX].properties;
+
+    let header = data.mfData.header;
+    await header.shift();
+    header.unshift("Date", "Folio", "Scheme Code / Name", "Unit Balance");
+    header.pop(); // remove transact column
+    await addHeaders(mfSheetInfo, data.mfData.header);
+
+    for (const mfData of data.mfData.data) {
+        let first = mfData.shift().split("\n"); //8552603\n123D / Kotak Emerging Equity Fund Dir-Gr\n\n[Unit Balance 71.786]
+        let unitBalance = first[3].replace(/[^\d.-]/g,'');
+        let fundName = first[1];
+        let folio = first[0];
+        mfData.unshift(datetime.toLocaleDateString('en-IN'), folio, fundName, unitBalance);
+        mfData.pop(); // remove transact column
+
+        let pk = mfData[0] + "-" + folio + "-" + fundName;
+        log("Adding mf data for.. " + pk)
+        await addData(mfSheetInfo, mfData, pk);
+    }
+    log("Done updating spreadsheet..");
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -26,25 +26,33 @@ function getBrowser() {
         '--user-agent="' + userAgent + '"'
     ];
 
-    // return puppeteer.launch({args: args, slowMo: slowMo, headless: false , devtools: true, defaultViewport: null});
+    // return puppeteer.launch({args: args, slowMo: slowMo, headless: false, defaultViewport: null});
     return puppeteer.launch({args: args, slowMo: slowMo});
 }
 
 async function getCamsData(retryCount, page) {
-    // return  [
-    //     '₹12,83,808.88',
-    //     'Total Cost Value',
-    //     '₹17,29,473.61',
-    //     'Total Market Value',
-    //     '₹6,54,664.73',
-    //     'Appreciation',
-    //     '667',
-    //     'Wtd. Avg Age days',
-    //     '9.65%',
-    //     'Wtd.Avg Annualized return*'
-    // ];
+    // return {
+    //     aggregateData:["₹17,48,808.88","Total Cost Value","₹23,57,944.17","Total Market Value","₹6,09,135.29","Appreciation","707","Wtd. Avg Age days","16.80%","Wtd.Avg Annualized return*"],
+    //     mfData:{
+    //         data: [
+    //             ["16955531\nD103G / SBI Blue Chip Fund Dir Plan-G\n\n[Unit Balance 6023.648]","248,487.73","388,493.37","140,005.64","1046","16.65%","      "],
+    //             ["16955531\nD040A / SBI Magnum Gilt Fund Direct Growth (formerly SBI Magnum Gilt Fund - Long Term)\n\n[Unit Balance 4908.984]","231,253.65","266,701.17","35,447.52","743","7.67%","      "],
+    //             ["16955531\nD038 / SBI Magnum Constant Maturity F D G (formerly SBI Magnum Gilt Fund - Short Term)\n\n[Unit Balance 2412.221]","115,710.22","126,029.62","10,319.40","668","5.00%","      "],
+    //             ["1018880674\n292GZ / ABSL Focused Equity Fund - Growth-DIRECT\n\n[Unit Balance 4789.436]","326,696.89","468,141.51","141,444.62","902","16.13%","      "],
+    //             ["11742512\nPREGT / HDFC Hybrid Equity Fund-Dir-Growth (formerly HDFC Premier Multi-Cap Fund, erstwhile HDFC Balanced Fund merged)\n\n[Unit Balance 3910.884]","216,570.94","326,801.29","110,230.35","983","16.17%","      "],
+    //             ["5783619\n785 / N R N E Fund - Dir - G\n\n[Unit Balance 4623.118]","170,000.00","277,502.66","107,502.66","586","37.88%","      "],
+    //             ["8549204\n124D / Kotak India EQ Contra Fund-Dir Plan-Gr\n\n[Unit Balance 1052.697]","95,000.00","96,529.16","1,529.16","69","8.87%","      "],
+    //             ["8549204\n104D / Kotak Small Cap- Direct Plan-Gr\n\n[Unit Balance 338.035]","60,000.00","60,652.94","652.94","107","3.85%","      "],
+    //             ["8549204\n123D / Kotak Emerging Equity Fund Dir-Gr\n\n[Unit Balance 575.621]","45,000.00","45,193.73","193.73","139","1.15%","      "],
+    //             ["8552603\n123D / Kotak Emerging Equity Fund Dir-Gr\n\n[Unit Balance 71.786]","5,000.00","5,636.13","636.13","286","16.58%","      "],
+    //             ["9527380\n8176 / Value Discovery Fund - DP Growth\n\n[Unit Balance 650.076]","120,089.45","176,723.16","56,633.71","751","21.09%","      "],
+    //             ["1000291329\nMBYDG / Mahindra Manulife MultiCap BY Dir-G\n\n[Unit Balance 5383.107]","115,000.00","119,539.43","4,539.43","86","18.12%","      "]
+    //         ],
+    //         header:["Folio\nScheme Code / Name","Cost Value (₹)","Market Value (₹)","Appreciation (₹)","Wtd. Avg Age days","Annualised XIRR","Transact"]
+    //     }
+    // };
 
-    let camsData = null;
+    let aggregateData = null;
     if(retryCount >= 5) {
         log(`retried ${retryCount} times stopping now..`)
         return;
@@ -54,7 +62,7 @@ async function getCamsData(retryCount, page) {
         waitUntil: "networkidle2"
     });
     await page.$eval("#UserId", (el, username) => {el.value =  username}, process.env.USERNAME)
-    camsData = await page.click("#btnNxt a").then(async function (){
+    aggregateData = await page.click("#btnNxt a").then(async function (){
         log("inside first btn click");
         await page.$eval("#PassWord", (el, pwd) => {el.value = pwd}, process.env.PASSWORD);
         log("pwd done");
@@ -87,23 +95,63 @@ async function getCamsData(retryCount, page) {
         }
 
         let div = await page.waitForSelector('#MutualFundDetails .row.mr-30')
-        camsData = await div.evaluate(div => div.innerText.split("\n"));
-
-        // logout
-        await (await page.$("a[ng-click='LogOutSession()']")).evaluate(b => b.click());
-        log("logged out and done..")
-        return camsData;
+        aggregateData = await div.evaluate(div => div.innerText.split("\n"));
+        return aggregateData;
     });
-    if(!camsData){
-        log("camsData is null..");
-        await takeScreenShot(page, "mycams-error-camsdata-null");
+    if(!aggregateData){
+        log("aggregateData is null..");
+        await takeScreenShot(page, "mycams-error-aggregateData-null");
+        return aggregateData;
     }
 
-    return camsData;
+    async function getMFData(page) {
+        let result = {data: []};
+        let mfCompanies = await page.$$("table#tblAMCDetails > tbody > tr.active");
+        log("mfCompanies : " + mfCompanies);
+        for (let mfCompany of mfCompanies) {
+
+            await mfCompany.click();
+            try {
+                await page.waitForSelector('#trSchemeDetails tr.active');
+            } catch (e) {
+                log("Error in getting mf data, trying one more time.. => " + JSON.stringify(e));
+                await mfCompany.click();
+                await mfCompany.click();
+                await page.waitForSelector('#trSchemeDetails tr.active');
+            }
+
+            result.header = await page.$$eval('#trSchemeDetails tr.card2 th.hidden-sm-down', headers => headers.map(header => header.innerText));
+
+            let rows = await page.$$eval('#trSchemeDetails tr.active', rows => {
+                return rows.map(row => {
+                    const columns = row.querySelectorAll('td');
+                    return Array.from(columns, column => column.innerText);
+                });
+            });
+            result.data = result.data.concat(rows);
+            // log("rows : " + JSON.stringify(rows));
+            // log("result : " + JSON.stringify(result));
+        }
+        return result;
+    }
+
+    let mfData = await getMFData(page);
+
+    // logout
+    try {
+        await (await page.$("a[ng-click='LogOutSession()']")).evaluate(b => b.click());
+        await (await page.$("#divLogout button.close")).evaluate(b => b.click());
+        log("logged out and done..");
+    } catch (e) {
+        log("Continuing inspite of error logging out => ", e);
+    }
+
+    return {aggregateData : aggregateData, mfData: mfData};
 }
 
 function appendToFile(datetime, numbers) {
-    let numbersString = numbers.join(", ") + "\n";
+    let numbersString = numbers.aggregateData.join(", ") + "\n";
+    // numbersString += JSON.stringify(numbers.mfData, null, 2) + "\n";
     fs.appendFileSync(os.homedir() + '/camsdata.txt',  datetime/1000 + ", " + numbersString);
 }
 
@@ -111,12 +159,14 @@ function appendToFile(datetime, numbers) {
     let browser = await getBrowser();
     let page = await browser.newPage();
     try {
-        const numbers = await getCamsData(1, page);
+        const camsData = await getCamsData(1, page);
         let datetime = Date.now();
-        log("Numbers: ", numbers);
-        appendToFile(datetime, numbers)
-        await sendToTelegram(datetime, numbers)
-        await addToGoogleSheet(new Date(datetime), numbers)
+        log("camsData: ", JSON.stringify(camsData));
+        appendToFile(datetime, camsData)
+        await sendToTelegram(datetime, camsData.aggregateData)
+        let d = new Date(datetime)
+        // d.setDate(d.getDate() + 24)
+        await addToGoogleSheet(d, camsData)
     } catch (e) {
         log(e);
         await takeScreenShot(page, `mycams-error-${e.message}`);
